347693153c25f57d14585bc595c2c56840889118
mirazizbekiminov <mirazizbekiminov951@gmail.com>
2026-09-05 13:21:45 +0500

    Dockerfile sintaksisi to'g'rilandi

--- config/urls.py
+++ config/urls.py
@@ -7,15 +7,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 
-    path(
-        "admin/",
-        admin.site.urls
-    ),
+    path("admin/", admin.site.urls ),
 
-    path(
-        "api/",
-        include("rest.urls")
-    ),
+    path("api/", include("rest.urls") ),
 
 ]
 
